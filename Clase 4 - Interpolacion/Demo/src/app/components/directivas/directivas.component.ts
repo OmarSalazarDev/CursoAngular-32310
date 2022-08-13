ed53602fa610dc083296824501fce3c90b985e45
Omar Salazar R <fs.f4system@gmail.com>
2022-08-13 14:07:57 -0500

    Day 4 - [Binding&Pipes]

--- Clase 4 - Interpolacion/Demo/src/app/components/directivas/directivas.component.ts
+++ Clase 4 - Interpolacion/Demo/src/app/components/directivas/directivas.component.ts
@@ -9,10 +9,24 @@ export class DirectivasComponent implements OnInit {
   variable1: number = 10;
   variable2: string = 'Abner';
   arreglo: string[] = ['Item 1 *ngFor', 'Item 2 *ngFor', 'Item 3 *ngFor'];
+  familiares = [
+    {
+      nombre: 'Abner',
+      apellido: 'Garcia'
+    },{
+      nombre: 'Jeisson',
+      apellido: 'Cortes'
+    }
+  ];
+  ahora = Date.now();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarVariable1() {
+    this.variable1 = Math.round(Math.random() * 10);
   }
 
 }
