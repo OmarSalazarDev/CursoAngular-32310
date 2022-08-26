a9680dce8e77a429e45846208e275075bb7eefc6
Omar Salazar R <fs.f4system@gmail.com>
2022-08-25 19:11:38 -0500

    Day 5 - [ComunicacionComponentes]

---
+++ Clase 5 - Comunicacion Componentes/Demo/src/app/components/hijo1/hijo1.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo1',
+  templateUrl: './hijo1.component.html',
+  styleUrls: ['./hijo1.component.css']
+})
+export class Hijo1Component implements OnInit {
+  @Input() listaEstudiantesHijo: string[] = [];
+  @Output() eventoSalida: EventEmitter<any> = new  EventEmitter<any>();
+  textoRecibido: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  obtenerEventoSalida(texto: any) {
+    this.eventoSalida.emit(texto.mensaje);
+    this.textoRecibido = texto;
+    console.log(this.textoRecibido);
+  }
+
+  }
