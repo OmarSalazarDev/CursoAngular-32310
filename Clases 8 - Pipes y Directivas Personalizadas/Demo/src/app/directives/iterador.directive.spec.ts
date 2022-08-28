3922c1405330578499f24bc891ede98bc84de606
Omar Salazar R <105831380+OmarSalazarDev@users.noreply.github.com>
2022-08-28 09:21:57 -0500

    Delete Clases 8 - Pipes y Directivas Personalizadas directory

--- Clases 8 - Pipes y Directivas Personalizadas/Demo/src/app/directives/iterador.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IteradorDirective } from './iterador.directive';
-
-describe('IteradorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IteradorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
