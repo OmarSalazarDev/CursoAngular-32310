3a8f20bcefff276dc8ac0b97913a77392a8e6480
Omar Salazar R <fs.f4system@gmail.com>
2022-10-10 22:34:44 -0500

    PROYECTOFINAL - PFSalazar

--- Proyecto Final/PFSalazar/src/app/app.component.spec.ts
+++ Proyecto Final/PFSalazar/src/app/app.component.spec.ts
@@ -2,11 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('Pruebas unitarias [AppComponent]', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -14,22 +17,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('El componente [AppComponent] se creó correctamente.', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'PFSalazar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PFSalazar');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PFSalazar app is running!');
   });
 });
